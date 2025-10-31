4b141cc496f2912ab9bb9e32fc97d29595744e08
Notnox <llrricardo@gmail.com>
2025-10-31 19:51:24 -0300

    feat: possibilidade de selecionar outras receitas de cantaria

--- src/pages/CantariaPage.tsx
+++ src/pages/CantariaPage.tsx
@@ -27,7 +27,6 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import {
   itemInfoMap,
   type ItemInfo,
-  recipeMap,
   cantariaData,
 } from '../data/cantariaData';
 import { calculateRequirements, type Requirements } from '../utils/craftCalculator';
